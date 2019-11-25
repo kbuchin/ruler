397fae3080e05a494f55082a00d9c0594c64e56e
Unknown <jobvdl@hotmail.com>
2019-11-25 20:26:26 +0100

    added stub for art gallery guarded vertex guards controler

---
+++ unity/Assets/Scripts/ArtGallery/Controller/ArtGalleryGuarderVertexGuardsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using General.Controller;
+using UnityEngine;
+
+namespace Assets.Scripts.ArtGallery.Controller
+{
+
+    using General.Controller;
+    using General.Menu;
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using UnityEngine.UI;
+    using Util.Algorithms.Polygon;
+    using Util.Geometry.Polygon;
+    using Util.Math;
+
+    /// <summary>
+    /// Main controller for the art gallery guarded vertex guards game.
+    /// Handles the game update loop, as well as level initialization and
+    /// advancement.
+    /// </summary>
+    public class ArtGalleryGuarderVertexGuardsController : MonoBehaviour, IController
+    {
+        /// <inheritdoc />
+        public void InitLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void CheckSolution()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void AdvanceLevel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
