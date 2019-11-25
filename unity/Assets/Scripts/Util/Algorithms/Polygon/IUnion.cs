b904ddbb2bcd6317769aa6284bb7c25ddf0dc2bf
Unknown <jobvdl@hotmail.com>
2019-11-25 20:05:58 +0100

    added interface for the union method

---
+++ unity/Assets/Scripts/Util/Algorithms/Polygon/IUnion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Util.Geometry;
+using Util.Geometry.Polygon;
+
+namespace Assets.Scripts.Util.Algorithms.Polygon
+{
+    /// <summary>
+    /// This interface defines methods that can be used to calculate the union
+    /// of polygons
+    /// </summary>
+    public interface IUnion
+    {
+        /// <summary>
+        /// Calculates the union of <paramref name="polygons"/>
+        /// </summary>
+        /// <param name="polygons">
+        /// The polygons to calculate the union of
+        /// </param>
+        /// <returns>A new polygon which is the union of the polygons defined in
+        /// <paramref name="polygons"/></returns>
+        IPolygon2D Union(ICollection<IPolygon2D> polygons);
+
+    }
+}
