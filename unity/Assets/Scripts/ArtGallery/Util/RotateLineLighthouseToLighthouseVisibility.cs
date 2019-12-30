2fe17d460444a79fc6a783cf724b92b08880b2e6
Mr Turtle <jobvdl@hotmail.com>
2019-12-30 12:11:32 +0100

    added smart guard visibility class

---
+++ unity/Assets/Scripts/ArtGallery/Util/RotateLineLighthouseToLighthouseVisibility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.ArtGallery.Util
+{
+    public static class RotateLineLighthouseToLighthouseVisibility
+    {
+
+    }
+}
